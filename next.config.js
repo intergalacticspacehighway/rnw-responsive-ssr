d4449b0cb24584471f8c3cbe20824fc1bc02d0aa
Nishan <nishanbende@gmail.com>
2022-08-11 18:13:22 +0530

    init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
+const withPlugins = require("next-compose-plugins");
+const withTM = require("next-transpile-modules")([
+  "react-native-responsive-query",
+]);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Transform all direct `react-native` imports to `react-native-web`
+      "react-native$": "react-native-web",
+    };
+    config.resolve.extensions = [
+      ".web.js",
+      ".web.jsx",
+      ".web.ts",
+      ".web.tsx",
+      ...config.resolve.extensions,
+    ];
+    return config;
+  },
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPlugins([withTM], nextConfig);
